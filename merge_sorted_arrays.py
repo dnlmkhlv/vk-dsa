c9cd9a783b99db2dc9a482e66c739bbbdc2f8033
dnlmkhlv <dan18082@mail.ru>
2025-06-25 16:09:02 +0400

    merge two sorted arrays

---
+++ merge_sorted_arrays.py
@@ -0,0 +1,22 @@
+def merge_sorted_arrays(arr1, arr2):
+    merged_arr = []
+    i = j = 0
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            merged_arr.append(arr1[i])
+            i += 1
+        if arr1[i] > arr2[j]:
+            merged_arr.append(arr2[j])
+            j += 1
+
+    merged_arr.extend(arr1[i:])
+    merged_arr.extend(arr2[j:])
+
+    return merged_arr
+
+arr1 = [1, 4, 9]
+arr2 = [2, 6, 8]
+merged_arr = merge_sorted_arrays(arr1, arr2)
+
+print(merged_arr)
