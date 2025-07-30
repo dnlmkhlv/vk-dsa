3a546e27f092b7a4a656a0800eb95ac43f71c7b1
dnlmkhlv <dan18082@mail.ru>
2025-07-30 22:22:06 +0400

    organized

--- two-pointers/merge_sorted_arrays.py
+++ two-pointers/merge_sorted_arrays.py
@@ -6,14 +6,19 @@ def merge_sorted_arrays(arr1, arr2):
         if arr1[i] < arr2[j]:
             merged_arr.append(arr1[i])
             i += 1
-        if arr1[i] > arr2[j]:
+        elif arr1[i] > arr2[j]:
             merged_arr.append(arr2[j])
+            j += 1
+        else:
+            merged_arr.append(arr1[i])
+            i += 1
             j += 1
 
     merged_arr.extend(arr1[i:])
     merged_arr.extend(arr2[j:])
 
     return merged_arr
+
 
 arr1 = [1, 4, 9]
 arr2 = [2, 6, 8]
